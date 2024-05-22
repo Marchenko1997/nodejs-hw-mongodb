f1a422a8d672e8880118e3265f7fd8fb1b8eeb9e
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-05-22 16:11:12 +0200

    Commitr 2d

---
+++ src/db/services/contacts.js
@@ -0,0 +1,12 @@
+ // src/services/students.js
+ import { Contacts } from '../models/contacts.js';
+
+ export const getAllContacts = async () => {
+   const contacts = await Contacts.find();
+   return contacts;
+ };
+
+ export const getContactById = async (contactId) => {
+   const contact = await Contacts.findById(contactId);
+   return contact;
+ };
--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import {env} from './utils/env.js';
 
+import {getAllContacts , getContactById} from './db/services/contacts.js';
+
 dotenv.config();
 
 const PORT = Number(env('PORT', '3000'));
@@ -22,6 +24,25 @@ export const setUpServer = () => {
       },
     }),
   );
+
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({ data: contacts, message: 'Successfully found contacts!' });
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+      res.status(200).json({ data: contact, message: 'Successfully found contact with id {contactId}!' });
+    } catch (error) {
+      res.status(404).json({ message: 'Something went wrong', error: error.message });
+    }
+  });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
