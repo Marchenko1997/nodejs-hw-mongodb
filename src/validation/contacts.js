3d9e2a14f6baae17dece1594c2ce5d06706d0b22
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-06-10 10:20:28 +0200

    Commite after mentors request

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,9 +5,9 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
-    email: Joi.string().min(3).max(20).email().required(),
-    isFavourite: Joi.boolean().required(),
-    contactType: Joi.string().valid('work', 'home', 'personal').required(),
+    email: Joi.string().min(3).max(20).email(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal'),
   });
 
 
