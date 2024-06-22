891784a473d7391149693488c85e545233d06280
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-06-22 20:54:42 +0200

    fx 7

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,16 +1,4 @@
-// import {Router} from "express";
-// import { authRouter } from "./auth.js";
-// import { contactsRouter } from "./contacts.js";
-
-// const router = Router();
-
-// router.use('/contacts', contactsRouter);
-// router.use('/auth', authRouter);
-
-// export default router;
-
-// src/routers/index.js
-import { Router } from "express";
+import {Router} from "express";
 import { authRouter } from "./auth.js";
 import { contactsRouter } from "./contacts.js";
 
@@ -19,8 +7,5 @@ const router = Router();
 router.use('/contacts', contactsRouter);
 router.use('/auth', authRouter);
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: "Your service is live 🎉" });
-});
-
 export default router;
+
